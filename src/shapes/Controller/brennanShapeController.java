21f3565731c0cfa829832aff37116aa1a184ad2f
Brennan Litster <brennan@thatoneplace.net>
2015-01-05 13:42:15 -0700

    Fixed stuff

--- src/shapes/Controller/brennanShapeController.java
+++ src/shapes/Controller/brennanShapeController.java
@@ -1,6 +1,26 @@
 package shapes.Controller;
 
+import lejos.nxt.Button;
+import shapes.Model.brennanDrawsRobot;
+
 public class brennanShapeController
 {
+	private brennanDrawsRobot debugBot;
 	
+	public brennanShapeController()
+	{
+		debugBot = new brennanDrawsRobot();
+	}
+	
+	public void start()
+	{
+		while(Button.waitForAnyPress() != Button.ID_ESCAPE)
+		{
+			debugBot.drawCircle(1, 1);
+			debugBot.drawSquare(4, 1);
+			debugBot.drawTriangle(3, 1);
+			debugBot.drawHexagon(6, 1);
+			debugBot.drawOctagon(8, 1);
+		}
+	}
 }
--- src/shapes/Model/brennanDrawsRobot.java
+++ src/shapes/Model/brennanDrawsRobot.java
@@ -4,7 +4,7 @@ import lejos.nxt.Motor;
 
 public class brennanDrawsRobot
 {
-	public void drawSahpe(int numberOfSides, int length)
+	public void drawCircle(int numberOfSides, int length)
 	{
 		int angle = calculateAngle(numberOfSides);
 		
@@ -12,6 +12,7 @@ public class brennanDrawsRobot
 		{
 			try
 			{
+				System.out.print("Circle");
 				Motor.A.rotate(angle, true);
 				Motor.B.flt();
 				Motor.C.backward();
@@ -27,6 +28,136 @@ public class brennanDrawsRobot
 			}
 		}
 	}
+	
+	public void drawSquare(int numberOfSides, int length)
+	{
+		
+		int angle = calculateAngle(numberOfSides);
+		
+		for(int drawCount =0; drawCount < numberOfSides; drawCount++)
+		{
+			try
+			{
+				System.out.print("Square");
+				Motor.B.backward();
+				Motor.C.backward();
+				Thread.sleep(length* 1000);
+				Motor.B.stop();
+				Motor.C.stop();
+				Motor.A.rotate(angle,true);
+				Thread.sleep(length* 1000);
+				Motor.C.backward();
+				Motor.B.forward();
+				Thread.sleep(length* 1000);
+				Motor.A.rotate(90, true);
+				Motor.B.stop();
+				Motor.C.stop();
+				Thread.sleep(length* 1000);	
+			}
+			catch(Exception threadException)
+			{
+				System.out.print(threadException.getMessage());
+			}
+		}	
+	}
+	
+	public void drawTriangle(int numberOfSides, int length)
+	{
+		
+		int angle = calculateAngle(numberOfSides);
+		
+		for(int drawCount =0; drawCount < numberOfSides; drawCount++)
+		{
+			try
+			{
+				System.out.print("Triangle");
+				Motor.B.backward();
+				Motor.C.backward();
+				Thread.sleep(length* 1000);
+				Motor.B.stop();
+				Motor.C.stop();
+				Motor.A.rotate(angle,true);
+				Thread.sleep(length* 1000);
+				Motor.C.backward();
+				Motor.B.forward();
+				Thread.sleep(length* 1600);
+				Motor.A.rotate(90, true);
+				Motor.B.stop();
+				Motor.C.stop();
+				Thread.sleep(length* 1000);
+			}
+			catch(Exception threadException)
+			{
+				System.out.print(threadException.getMessage());
+			}
+		}	
+	}
+	
+	public void drawHexagon(int numberOfSides, int length)
+	{
+		
+		int angle = calculateAngle(numberOfSides);
+		
+		for(int drawCount =0; drawCount < numberOfSides; drawCount++)
+		{
+			try
+			{
+				System.out.print("Hexagon");
+				Motor.B.backward();
+				Motor.C.backward();
+				Thread.sleep(length* 1000);
+				Motor.B.stop();
+				Motor.C.stop();
+				Motor.A.rotate(angle, true);
+				Thread.sleep(length* 1000);
+				Motor.C.backward();
+				Motor.B.forward();
+				Thread.sleep(length* 800);
+				Motor.B.stop();
+				Motor.C.stop();
+				Motor.A.rotate(90, true);
+				Thread.sleep(length* 1000);	
+			}
+			catch(Exception threadException)
+			{
+				System.out.print(threadException.getMessage());
+			}
+		}
+	}
+	
+	public void drawOctagon(int numberOfSides, int length)
+	{
+		
+		int angle = calculateAngle(numberOfSides);
+		
+		for(int drawCount =0; drawCount < numberOfSides; drawCount++)
+		{
+			try
+			{
+				System.out.print("Octagon");
+				Motor.B.backward();
+				Motor.C.backward();
+				Thread.sleep(length* 1000);
+				Motor.B.stop();
+				Motor.C.stop();
+				Motor.A.rotate(angle, true);
+				Thread.sleep(length* 1000);
+				Motor.C.backward();
+				Motor.B.forward();
+				Thread.sleep(length* 500);
+				Motor.B.stop();
+				Motor.C.stop();
+				Motor.A.rotate(angle, true);
+				Thread.sleep(length* 1000);
+					
+			}
+			catch(Exception threadException)
+			{
+				System.out.print(threadException.getMessage());
+			}
+		}
+	}
+	
 	
 	public int calculateAngle(int numberOfSides)
 	{

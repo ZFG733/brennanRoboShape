55d520332274680d2d8ca2b5ccf57648d2d35b18
Brennan Litster <brennan@thatoneplace.net>
2015-01-06 13:17:23 -0700

    Got Runner Finished

--- src/shapes/Controller/brennanShapesRunner.java
+++ src/shapes/Controller/brennanShapesRunner.java
@@ -4,7 +4,8 @@ public class brennanShapesRunner
 {
 	public static void main (String[] args)
 	{
-		System.out.println("Brennan");
+		brennanShapeController controller = new brennanShapeController();
+		controller.start();
 	}
 	
 }
